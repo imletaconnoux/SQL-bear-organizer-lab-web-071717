b8ae69f003ca579b7715822e31f15a0a0f8536c9
imletaconnoux <imletaconnoux@gmail.com>
2017-07-24 16:59:04 -0400

    passed tests

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocholate', 5, 'M', 'Brown', 'Sweet', 'TRUE'
+);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 3, 'M', 'Brown', 'Loud', 'TRUE'
+);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 7, 'F', 'White', 'Free Spirited', 'TRUE'
+);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 10, 'M', 'Brown', 'Strict', 'TRUE'
+);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 8, 'F', 'Black', 'Artistic', 'TRUE'
+);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 19, 'M', 'Green', 'Bitter', 'TRUE'
+);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 11, 'F', 'Red', 'Soft spoken', 'TRUE'
+);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 12, 'M', 'Black', 'Angry','TRUE'
+);
